d3baee3dce3666b27d76575b132f95dbb05d6523
Ricarte13 <viniciusdiv@hotmail.com>
2023-09-27 21:13:58 -0300

    Creating Post entity with neasted User

--- src/main/java/com/ricarte/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ricarte/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.ricarte.workshopmongo.config;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.ricarte.workshopmongo.domain.Post;
 import com.ricarte.workshopmongo.domain.User;
+import com.ricarte.workshopmongo.repository.PostRepository;
 import com.ricarte.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,18 +18,27 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, LocalDate.parse("21/03/2018", dtf), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, LocalDate.parse("23/03/2018", dtf), "Bom dia", "Acordei feliz hoje!", maria);
 		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
 
